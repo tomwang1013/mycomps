4e7509d2b001f94febd2f0277eb65940e8369190
tomwang <174604588@qq.com>
2017-03-11 18:24:09 +0800

    init

---
+++ index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  FaRating:         require('./lib/components/fa_rating.vue'),
+  InputLabels:      require('./lib/components/input_labels.vue'),
+  PopupList:        require('./lib/components/popup_list.vue'),
+  PopupOverlay:     require('./lib/components/popup_overlay.vue'),
+  PopupTabs:        require('./lib/components/popup_tabs.vue'),
+  SearchSuggestion: require('./lib/components/search_suggestion.vue')
+};
